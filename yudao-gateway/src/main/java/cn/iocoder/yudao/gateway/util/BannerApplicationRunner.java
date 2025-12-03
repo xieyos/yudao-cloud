e5409a3ce9a1006956f68436db208187952dbfb4
xieyos <xieyos@163.com>
2025-12-03 10:08:28 +0800

    添加注释

--- yudao-gateway/src/main/java/cn/iocoder/yudao/gateway/util/BannerApplicationRunner.java
+++ yudao-gateway/src/main/java/cn/iocoder/yudao/gateway/util/BannerApplicationRunner.java
@@ -22,7 +22,7 @@ public class BannerApplicationRunner implements ApplicationRunner {
         ThreadUtil.execute(() -> {
             ThreadUtil.sleep(1, TimeUnit.SECONDS); // 延迟 1 秒，保证输出到结尾
             log.info("""
-                    ----------------------------------------------------------
+                    \n----------------------------------------------------------
                     \t\
                     项目启动成功！
                     ----------------------------------------------------------""");
